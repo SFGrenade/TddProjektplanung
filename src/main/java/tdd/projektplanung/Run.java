76831eec3e720cd4c42f42f24ae7da8ff303673c
SFGrenade <5044221-SFGrenade@users.noreply.gitlab.com>
2021-06-04 10:40:23 +0200

    Add Saving/Loading Add Database Add function to make cli params of line Format document

--- src/main/java/tdd/projektplanung/Run.java
+++ src/main/java/tdd/projektplanung/Run.java
@@ -1,16 +1,46 @@
 package tdd.projektplanung;
 
-import java.text.DateFormat;
-import java.text.FieldPosition;
-import java.text.ParseException;
-import java.text.ParsePosition;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
 
 public class Run {
-	public static void main(String[] args) {
-		if (args[0].compareTo("e") == 0) {
+	public static String[] makeCliOfString(String line) {
+		List<String> ret = new LinkedList<String>();
+		
+		boolean inQuotes = false;
+		String currString = "";
+		for (int i = 0; i < line.length(); i++) {
+			char c = line.charAt(i);
+			if (!inQuotes && (c == ' ')) {
+				ret.add(currString);
+				currString = "";
+			}
+			else if (c == '"') {
+				inQuotes = !inQuotes;
+			}
+			else {
+				currString = String.format("%s%c", currString, c);
+			}
+		}
+		ret.add(currString);
+		
+		String[] finalRet = new String[ret.size()];
+		for (int i = 0; i < finalRet.length; i++) {
+			finalRet[i] = ret.get(i);
+		}
+		
+		return finalRet;
+	}
+	
+	public static void computeLine(String[] args) {
+		if (args.length > 0 && args[0].compareTo("e") == 0) {
 			// Adding Employee
 			Employee e = new Employee();
 			try {
@@ -26,10 +56,10 @@ public class Run {
 				e.remove();
 				System.out.println("Error parsing Employee data!");
 				System.out.println("Example:");
-				System.out.println("e \"firstname\" \"lastname\" \"email\" \"Telephone Number\" \"Street Name\" \"Street Number\" \"ZipCode\" \"Country Name\"");
+				System.out.println(
+						"e \"firstname\" \"lastname\" \"email\" \"Telephone Number\" \"Street Name\" \"Street Number\" \"ZipCode\" \"Country Name\"");
 			}
-		}
-		else if (args[0].compareTo("p") == 0) {
+		} else if (args.length > 0 && args[0].compareTo("p") == 0) {
 			// Adding Project
 			try {
 				SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -39,12 +69,11 @@ public class Run {
 				int hoursTotal = Integer.parseInt(args[4]);
 				Project p = new Project(name, startDate, endDate, hoursTotal);
 			} catch (Exception ex) {
-				System.out.println("Error parsing Project data!");
+				System.out.println(String.format("Error parsing Project data! '%s'", String.join(" ", args)));
 				System.out.println("Example:");
 				System.out.println("p \"Project Name\" \"2000-01-01\" \"2000-01-02\" \"1300\"");
 			}
-		}
-		else if (args[0].compareTo("ep") == 0) {
+		} else if (args.length > 0 && args[0].compareTo("ep") == 0) {
 			// Doing work with employees in projects
 			try {
 				if (args[1].compareTo("aep") == 0) {
@@ -60,17 +89,18 @@ public class Run {
 						for (int i = 0; i < Project.allProjects.size(); i++) {
 							if (Project.allProjects.get(i).getName().compareTo(projectName) == 0) {
 								Project toAdd = Project.allProjects.get(i);
-								toAdd.addEmployeeToProject(Employee.findEmployee(employeeFirstName, employeeLastName), productOwner, scrumMaster, backendDev, frontendDev, wantedHours);
+								toAdd.addEmployeeToProject(Employee.findEmployee(employeeFirstName, employeeLastName),
+										productOwner, scrumMaster, backendDev, frontendDev, wantedHours);
 								break;
 							}
 						}
 					} catch (Exception e) {
 						System.out.println("Error parsing data to add Employee to Project!");
 						System.out.println("Example:");
-						System.out.println("ep aep \"Project Name\" \"Employee First Name\" \"Employee Last Name\" (product owner [false]) (scrum master [false]) (backend dev [false]) (frontend dev [false]) \"1300\"");
+						System.out.println(
+								"ep aep \"Project Name\" \"Employee First Name\" \"Employee Last Name\" (product owner [false]) (scrum master [false]) (backend dev [false]) (frontend dev [false]) \"1300\"");
 					}
-				}
-				else if (args[1].compareTo("ath") == 0) {
+				} else if (args[1].compareTo("ath") == 0) {
 					try {
 						String projectName = args[2];
 						String employeeFirstName = args[3];
@@ -79,17 +109,18 @@ public class Run {
 						for (int i = 0; i < Project.allProjects.size(); i++) {
 							if (Project.allProjects.get(i).getName().compareTo(projectName) == 0) {
 								Project toAdd = Project.allProjects.get(i);
-								toAdd.addTrackedHours(Employee.findEmployee(employeeFirstName, employeeLastName), workedHours);
+								toAdd.addTrackedHours(Employee.findEmployee(employeeFirstName, employeeLastName),
+										workedHours);
 								break;
 							}
 						}
 					} catch (Exception e) {
 						System.out.println("Error parsing data to add tracked hours to Employee in Project!");
 						System.out.println("Example:");
-						System.out.println("ep ath \"Project Name\" \"Employee First Name\" \"Employee Last Name\" \"7\"");
+						System.out.println(
+								"ep ath \"Project Name\" \"Employee First Name\" \"Employee Last Name\" \"7\"");
 					}
-				}
-				else if (args[1].compareTo("gfe") == 0) {
+				} else if (args[1].compareTo("gfe") == 0) {
 					try {
 						String projectName = args[2];
 						for (int i = 0; i < Project.allProjects.size(); i++) {
@@ -113,25 +144,83 @@ public class Run {
 				System.out.println("Example:");
 				System.out.println("ep aep");
 			}
-		}
-		else {
+		} else {
 			System.out.println("Get Started with Employees and Projects:");
 			System.out.println("e|p|ep");
 		}
-		
-		// ToDo SAVE STANDINGS
-		
+	}
+
+	public static void loadEverything() {
+		try {
+			BufferedReader myReader = new BufferedReader(new FileReader("database.txt"));
+			myReader.lines().forEach(line -> {
+				if (line.trim().length() > 0) {
+					computeLine(makeCliOfString(line));
+				}
+		    });
+			myReader.close();
+		} catch (IOException e) {
+			System.out.println("An error occurred while loading the database.");
+			e.printStackTrace();
+		}
+	}
+
+	public static void saveEverything() {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		List<String> lines = new LinkedList<String>();
+		for (Employee e : Employee.allEmployees) {
+			lines.add(String.format("e \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" \"%s\"", e.getFirstName(),
+					e.getLastName(), e.getEMail(), e.getTelephoneNumber(), e.getStreetName(), e.getStreetNumber(),
+					e.getZipCode(), e.getCountry()));
+		}
+		for (Project p : Project.allProjects) {
+			lines.add(String.format("p \"%s\" \"%s\" \"%s\" \"%s\"", p.getName(),
+					simpleDateFormat.format(p.getStartDate()), simpleDateFormat.format(p.getEndDate()),
+					p.getHoursTotal()));
+			for (Entry<Employee, EmployeeInProject> pair : p.getAssignedEmployees().entrySet()) {
+				lines.add(String.format("ep aep \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" \"%s\"", p.getName(),
+						pair.getKey().getFirstName(), pair.getKey().getLastName(), pair.getValue().isProductOwner(),
+						pair.getValue().isScrumMaster(), pair.getValue().isBackendDeveloper(),
+						pair.getValue().isFrontendDeveloper(), pair.getValue().getWantedHours()));
+				lines.add(String.format("ep ath \"%s\" \"%s\" \"%s\" \"%s\"", p.getName(), pair.getKey().getFirstName(),
+						pair.getKey().getLastName(), pair.getValue().getTrackedHours()));
+			}
+		}
+		try {
+			FileWriter myWriter = new FileWriter("database.txt");
+			for (String line : lines) {
+				myWriter.write(line);
+				myWriter.write("\n");
+			}
+			myWriter.close();
+			System.out.println("Databse saved.");
+		} catch (IOException e) {
+			System.out.println("An error occurred while saving the database.");
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		loadEverything();
+
+		if (args.length > 0)
+			computeLine(args);
+
+		saveEverything();
+
 		for (Employee e : Employee.allEmployees) {
 			System.out.println(String.format("Name: %s %s", e.getFirstName(), e.getLastName()));
 			System.out.println(String.format("Email: %s", e.getEMail()));
 			System.out.println(String.format("Tel: %s", e.getTelephoneNumber()));
-			System.out.println(String.format("Adress: %s %s, %s %s", e.getStreetName(), e.getStreetNumber(), e.getZipCode(), e.getCountry()));
+			System.out.println(String.format("Adress: %s %s, %s %s", e.getStreetName(), e.getStreetNumber(),
+					e.getZipCode(), e.getCountry()));
 			System.out.println();
 		}
 		for (Project p : Project.allProjects) {
 			System.out.println("Project:");
 			System.out.println(String.format("Name: %s", p.getName()));
-			System.out.println(String.format("Time Window: %s - %s", p.getStartDate().toString(), p.getEndDate().toString()));
+			System.out.println(
+					String.format("Time Window: %s - %s", p.getStartDate().toString(), p.getEndDate().toString()));
 			System.out.println(String.format("Total Hours: %d", p.getHoursTotal()));
 			System.out.println();
 		}
